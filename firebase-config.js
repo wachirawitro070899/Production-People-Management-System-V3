9835f43a12bf9aca787604c9be7bf43ad35b491f
wachirawitro070899 <wachirawit.ro070899@gmail.com>
2026-08-04 15:09:08 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
-// Firebase configuration for PPMS V314
-// 1) Create a Firebase project
-// 2) Enable Realtime Database
-// 3) Replace the values below with your Firebase Web App configuration
+// PPMS Firebase Realtime Database configuration
+// Project: JRTL-Organization / wachirawit-c8582
+// IMPORTANT: Create Realtime Database in Singapore (asia-southeast1) before use.
 window.PPMS_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyB4hIpI6bLI7L-CZ9JY0XjFnGrwTmVQ3bE",
+  authDomain: "wachirawit-c8582.firebaseapp.com",
+  databaseURL: "https://wachirawit-c8582-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wachirawit-c8582",
+  storageBucket: "wachirawit-c8582.firebasestorage.app",
+  messagingSenderId: "860073002133",
+  appId: "1:860073002133:web:65ecba09c0c3cd4702879c"
 };
